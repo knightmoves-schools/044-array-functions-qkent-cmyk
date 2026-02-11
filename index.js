50b802465b02a5dd0ce5108726959f2a1dd59f96
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-11 01:31:35 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var automobiles = ["car", "truck", "motorcycle"];
+
+automobiles.push('airplane', 'skateboard');
+
+automobiles.unshift('bike', 'helicopter');
+
+var automobilesCopy = `${automobiles}`;
+
+automobiles.pop();
+
+automobiles.shift();
+
+document.getElementById('result').innerHTML = automobiles;
 
